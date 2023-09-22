4d6c50df99ddfc8a4ae4ad148fb6233e2c273473
Nikifor <nik.slepcov1771@gmail.com>
2023-09-22 10:49:32 +0200

    not done yet

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     const [isComment , setIsComment] = useState([]);
     const [isTodoName, setIsTodoName] = useState('');
     const [isComName, setIsComName] = useState('')
+    const [activeId, setActiveId] = useState('');
     const [isColor , setIsColor] = useState('')
-
     const onChangeTodo = (event) => {
         setIsTodoName(event.target.value);
     }
@@ -25,9 +25,11 @@ function App() {
         setIsTodo(tmpTodo);
     }
     const addTodo = () => {
-        let newTodo = [...isTodo , isTodoName];
+        let tmpAdd = [...isTodo];
+        tmpAdd.push({id: Math.round(Math.random() * 100) , name: isTodoName});
+        console.log(tmpAdd);
         if (isTodoName !== '') {
-            setIsTodo(newTodo);
+            setIsTodo(tmpAdd);
         }
         setIsTodoName('');
 
@@ -62,14 +64,14 @@ function App() {
                 <ul className={'todo-items'}>
                     {isTodo.map((item ,index) => {
                         return(
-                            <li className={'todo-list_item'} key={index}>{item} <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="48px" height="48px" onClick={() => onDeleteTodo(index) }><path fill="#9575CD" d="M34,12l-6-6h-8l-6,6h-3v28c0,2.2,1.8,4,4,4h18c2.2,0,4-1.8,4-4V12H34z"/><path fill="#7454B3" d="M24.5 39h-1c-.8 0-1.5-.7-1.5-1.5v-19c0-.8.7-1.5 1.5-1.5h1c.8 0 1.5.7 1.5 1.5v19C26 38.3 25.3 39 24.5 39zM31.5 39L31.5 39c-.8 0-1.5-.7-1.5-1.5v-19c0-.8.7-1.5 1.5-1.5l0 0c.8 0 1.5.7 1.5 1.5v19C33 38.3 32.3 39 31.5 39zM16.5 39L16.5 39c-.8 0-1.5-.7-1.5-1.5v-19c0-.8.7-1.5 1.5-1.5l0 0c.8 0 1.5.7 1.5 1.5v19C18 38.3 17.3 39 16.5 39z"/><path fill="#B39DDB" d="M11,8h26c1.1,0,2,0.9,2,2v2H9v-2C9,8.9,9.9,8,11,8z"/></svg></li>
+                            <li onClick={() => {setActiveId(item.id)}} className={'todo-list_item'} key={index}>{item.name}<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="48px" height="48px" onClick={() => onDeleteTodo(index) }><path fill="#9575CD" d="M34,12l-6-6h-8l-6,6h-3v28c0,2.2,1.8,4,4,4h18c2.2,0,4-1.8,4-4V12H34z"/><path fill="#7454B3" d="M24.5 39h-1c-.8 0-1.5-.7-1.5-1.5v-19c0-.8.7-1.5 1.5-1.5h1c.8 0 1.5.7 1.5 1.5v19C26 38.3 25.3 39 24.5 39zM31.5 39L31.5 39c-.8 0-1.5-.7-1.5-1.5v-19c0-.8.7-1.5 1.5-1.5l0 0c.8 0 1.5.7 1.5 1.5v19C33 38.3 32.3 39 31.5 39zM16.5 39L16.5 39c-.8 0-1.5-.7-1.5-1.5v-19c0-.8.7-1.5 1.5-1.5l0 0c.8 0 1.5.7 1.5 1.5v19C18 38.3 17.3 39 16.5 39z"/><path fill="#B39DDB" d="M11,8h26c1.1,0,2,0.9,2,2v2H9v-2C9,8.9,9.9,8,11,8z"/></svg></li>
                         )
                     })}
                 </ul>
             </div>
         </article>
         <article className={'comments'}>
-            <p className={'comments-title'}>Comments #{Math.round(Math.random() * Math.pow(10, 10))}</p>
+            <p className={'comments-title'}>Comments #{activeId}</p>
             <form className={'comments-form'}>
                 <input onChange={onChangeColor} className={'comments-input-color'} type={"color"} value={isColor}/>
                 <input onChange={onChangeCom} className={'comments-input'} type={"text"} value={isComName} placeholder="Type comment here..." required />
